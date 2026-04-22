f995058db6d3ca66ad173f4c315619261bc0dec8
appl3bees <vegaroberto21@yahoo.com>
2026-04-22 18:51:53 -0400

    Created initial enviroment and added a VideoExceptions.py class to handle exceptions for. Will initially check that the camera is accessable. created a window to access the camera

--- main.py
+++ main.py
@@ -3,24 +3,52 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-import VideoExceptions
+import VideoExceptions #import custom exception class
 import cv2 # import the cv2 library
+from pynput import keyboard
+
 
 cap = cv2.VideoCapture(0) #Global variable: initialize the cap variable to the video caputre device
+#opencam = cv2.VideoCapture.open(cap)
 
-def getframe():
+
+def getframe(): #method to check if the frames are being captured called from main
     ret, frame = cap.read()
     try:
         if frame is None:
             raise VideoExceptions.FrameReadError()
     except VideoExceptions.FrameReadError as e:
         print("Error:", e)
-        return frame
+    return frame
 
-# Press the green button in the gutter to run the script.
+def getcamera(): #method to check if the camera is able can be accessed called from main
+    try:
+        if not cap.isOpened():
+            raise VideoExceptions.CameraNotFoundError()
+    except VideoExceptions.CameraNotFoundError as e:
+        print("Error:", e)
+        exit(1)
+
 if __name__ == '__main__':
-
-   # ret, frame = cap.read()
+    getcamera()
     getframe()
+
+   # apiID = cv2.getBackendName()
+    while True:
+        frame = getframe()
+
+        if frame is None:
+            break
+        cv2.imshow("Sign Languange Translator", frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+
+    cap.release()
+    cv2.destroyAllWindows()
+       # ret, frame = cap.read()
+
+        #opencam()
 
 
